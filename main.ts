42d953031adb5879dc8424bdadad46e5f25b950e
Galen Howlett <howlettga@gmail.com>
2024-06-26 11:12:12 -0400

    health

--- main.ts
+++ main.ts
@@ -1,18 +1,26 @@
+import express from "express";
 import { WordleBot } from "./bot";
 import { GoogleSheet } from "./google-sheets";
 
 console.log("Wordle bot booting...");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Healthy");
+});
 
 GoogleSheet.initAuth().then((auth) => {
   const sheet = new GoogleSheet(process.env.GOOGLE_SHEET_ID as string, auth);
   const bot = new WordleBot(sheet);
 
   bot.start();
-  console.log("Wordle bot running...");
+  app.listen(process.env.PORT || 8080, () => {
+    console.log("Wordle bot running...");
+  });
 }).catch((err) => {
   if (err.message === "INVALID_GOOGLE_AUTH_METHOD") {
     console.error("Invalid Google Auth Method Please set the required GOOGLE_AUTH_METHOD environment variable. Exiting...");
-    console.error(process.env);
     process.exit(1);
   } else {
     console.error(err);
